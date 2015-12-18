704f1cfb2538be0bb6eb47e3d586c49cedeba116
Rekha Nambrath <rekhanambrath@gmail.com>
2015-12-18 11:04:04 +0530

    moved common content to layout/application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   		@user = User.find_by_email(params[:session][:email])
   		if @user && @user.authenticate(params[:session][:password])
     			session[:user_id] = @user.id
-    			redirect_to '/welcome'
+    			redirect_to '/dashboard'
   		else
     			redirect_to 'login'
   		end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     		@user=User.new(user_params)
     		if @user.save
       			session[:user_id]=@user.id
-      			redirect_to '/welcome'
+      			redirect_to '/dashboard'
     		else
       			redirect_to '/signup'
     		end
