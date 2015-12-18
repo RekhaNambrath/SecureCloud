db0dc2beb55b348b3d9fb40cb03d5723ec7b7cd3
Rekha Nambrath <rekhanambrath@gmail.com>
2015-12-18 11:40:39 +0530

    added helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,6 @@ class UsersController < ApplicationController
   	def user_params
     		params.require(:user).permit(:first_name,:last_name, :email, :password)
   	end
+  def dashboard
+  end
 end
