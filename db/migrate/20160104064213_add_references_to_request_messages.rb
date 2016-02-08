f979dbf64165876eadabe9aaec072e41c1a89446
Hari K <hari01234@gmail.com>
2016-02-08 11:11:03 +0530

    Commit on 8th Feb to resolve db conflict

--- db/migrate/20160104064213_add_references_to_request_messages.rb
+++ db/migrate/20160104064213_add_references_to_request_messages.rb
@@ -1,4 +1,4 @@
-class AddFilesToRequestMessages < ActiveRecord::Migration
+class AddReferencesToRequestMessages < ActiveRecord::Migration
   def change
     add_reference :request_messages, :user, index: true, foreign_key: true
     add_reference :request_messages, :file_upload, index: true, foreign_key: true
